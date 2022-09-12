16817068807d32e2c242258e1620de63a6fdf4c2
Daniel Zirat <zirat@student.tugraz.at>
2022-09-12 15:04:58 +0300

    test

---
+++ main.py
@@ -0,0 +1,47 @@
+'''
+A linear regression learning algorithm example using TensorFlow library.
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy
+import matplotlib.pyplot as plt
+import tensorflowjs as tfjs
+rng = numpy.random
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 1000
+display_step = 50
+
+# Training Data
+X_train = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
+                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
+y_train = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
+                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+
+# Testing example, as requested (Issue #2)
+X_test = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
+y_test = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
+
+inputs = tf.keras.layers.Input(shape=(1,))
+outputs = tf.keras.layers.Dense(1)(inputs)
+model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer="Adam", loss="mse", metrics=["mae"])
+
+model.fit(X_train, y_train,
+          batch_size=64,
+          epochs=10,
+          verbose=1,
+          validation_data=(X_test, y_test))
+
+
+model.save('saved_model/my_model')
+
+print(model.predict(2))
+
+
+tfjs.converters.save_keras_model(model, 'saved_model/my_model')
